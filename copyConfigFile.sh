7c62c33f22d56430ba79a0196477a8d7fe84cd7b
mang <ningma003@gmail.com>
2017-11-07 15:05:18 +0800

    修改README

--- copyConfigFile.sh
+++ copyConfigFile.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 #功能描述：备份mac上常用的配置文件
 
-#DEST=/Users/mang/AppData/Dropbox/mac/macConfigBackup
-#BASE_PATH=$(cd $(dirname "$0");pwd)
 
-#DEST=/Users/mang/AppData/百度云同步盘/mac/macConfigBackup/shellConfigBak
 DEST=~/.mac.config
 cp ~/.vim/.vimrc  $DEST/
 cp ~/.gvimrc  $DEST/
